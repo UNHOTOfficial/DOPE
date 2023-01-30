8a5f0aac06e5441e7b2fe131aa438ad22f8c588c
Unhot <serverings@protonmail.com>
2023-01-31 03:06:46 +0330

    Add Layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
-import './globals.css'
+import Footer from "@/components/Footer";
+import HeaderPrimary from "@/components/HeaderPrimary";
+import HeaderSecondary from "@/components/HeaderSecondary";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +15,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <HeaderPrimary />
+        <HeaderSecondary />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }

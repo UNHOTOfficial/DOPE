ce06d128149ca3c79bfa3c619111eabadef6f8e3
Unhot <serverings@protonmail.com>
2023-01-31 03:45:53 +0330

    Remove Default Font.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import Slider from "@/components/Slider";
 import ScrollMenu from "@/components/ScrollMenu";
 import ProductCard from "@/components/ProductCard";
 import CategoriesItems from "@/components/CategoriesItems";
 import NewsCard from "@/components/NewsCard";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
   const productsData = await fetch("https://dope-red.vercel.app/api/products");

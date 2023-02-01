6939ddaebae56e56aadf603f15fb703ecf280567
Unhot <serverings@protonmail.com>
2023-02-02 02:15:54 +0330

    Fix Title.

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Dope - Fashion And</title>
+      <title>Dope - Fashion And Style</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Dope - Fashion And Style Online Shop." />
       <link rel="icon" href="/favicon.ico" />

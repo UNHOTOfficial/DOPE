255df1f38b743ec0cb75bcfa7ae66dd98273663c
Unhot <serverings@protonmail.com>
2023-02-02 02:16:41 +0330

    Make Title Bigger.

--- components/ScrollMenu.tsx
+++ components/ScrollMenu.tsx
@@ -5,7 +5,7 @@ import ProductCard from "./ProductCard";
 export default function ScrollMenu({ title, children }: any) {
   return (
     <div className="flex flex-col">
-      <h1 className="text-center py-2 font-bold">{title}</h1>
+      <h1 className="text-center py-2 font-bold text-lg">{title}</h1>
       <div className="grid overflow-auto gap-2 px-2 grid-flow-col">
         {children}
       </div>

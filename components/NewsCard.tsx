47911f05f29211e0478714a2d7ef3d07d7e2b860
Unhot <serverings@protonmail.com>
2023-02-02 04:28:42 +0330

    Fix Bug.

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -14,11 +14,17 @@ export default function NewsCard({
       <Image src={photo} alt={title} width={300} height={250} />
       <span>{title}</span>
       <div className="flex flex-row">
-        <Image className="rounded-full" src={authorPhoto} alt={author} width={50} height={50} />
+        <Image
+          className="rounded-full"
+          src={authorPhoto}
+          alt={author}
+          width={50}
+          height={50}
+        />
         <span>{author}</span>
       </div>
       <p>{overview}</p>
-      <span>{readingTime}</span>
+      <span>{readingTime} Minutes</span>
     </div>
   );
 }

95c2adb57a60764185b54a53227f42aa4b9859cf
Unhot <serverings@protonmail.com>
2023-02-02 04:56:05 +0330

    Add Theme Color.

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Dope - Fashion And Style Online Shop." />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#ff4057" />
     </>
   )
 }

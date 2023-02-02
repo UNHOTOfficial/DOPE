5c255e009ae1a0a2c855e2af541f2096f686fd44
Unhot <serverings@protonmail.com>
2023-02-02 04:28:10 +0330

    Add Product Page.

--- app/[product]/page.tsx
+++ app/[product]/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import Button from "@/components/Button";
+import ProductColors from "@/components/ProductColors";
+import Sizes from "@/components/Sizes";
+import fetcher from "@/services/fetch";
+import Image from "next/image";
+import React from "react";
 
-export default function page() {
+export default async function page(context: any) {
+  const id = context.params.product;
+  const productArray = await fetcher(
+    `https://dope-red.vercel.app/api/products/${id}`
+  );
+  const product = productArray[0];
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <Image src={product.photo} alt={product.title} width={300} height={250} />
+      <h1>{product.title}</h1>
+      {/* <div><span>Colors:</span><ProductColors Colors={product?.colors}/></div> */}
+      <div>
+        <span>Sizes:</span>
+        <Sizes />
+      </div>
+      <p>{product.overview}</p>
+      <div className="flex flex-col w-full items-stretch">
+        <Button addedClass={"w-80"} data="Add To Cart" type="button" />
+        <Button addedClass={"w-80"} data="Add To Wishlist" type="button" />
+      </div>
+    </div>
+  );
 }

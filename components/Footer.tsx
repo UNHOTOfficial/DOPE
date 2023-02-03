872e237c019bf865100d52e38bbc89df9fc86a22
UNHOT <Serverings@protonmail.com>
2023-02-03 09:10:38 -0800

    Overall Fixes.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,33 +4,33 @@ import React from "react";
 import Logo from "../public/Logo.svg";
 export default function Footer() {
   return (
-    <div className="flex flex-col my-4 border-t border-slate-600">
-      <div className="grid grid-cols-2 gap-4 p-4">
-        <div className="flex flex-col">
-          <span>Company</span>
-          <span>About</span>
-          <span>Contact</span>
-          <span>Careers</span>
-          <span>Blog</span>
+    <div className="flex flex-col my-4 border-t border-slate-700">
+      <div className="grid grid-cols-2 gap-4 p-4 text-gray-500">
+        <div className="flex flex-col space-y-1 max-w-fit">
+          <span className="text-slate-100 font-semibold">Company</span>
+          <Link className="hover:underline" href={"/about"}>About</Link>
+          <Link className="hover:underline" href={"/contact"}>Contact</Link>
+          <Link className="hover:underline" href={"/careers"}>Careers</Link>
+          <Link className="hover:underline" href={"/blog"}>Blog</Link>
         </div>
-        <div className="flex flex-col">
-          <span>Help Center</span>
-          <span>FAQs</span>
-          <span>Guides</span>
-          <span>Order Tracking</span>
-          <span>Warranty</span>
+        <div className="flex flex-col space-y-1 max-w-fit">
+          <span className="text-slate-100 font-semibold">Help Center</span>
+          <Link className="hover:underline" href={"/faqs"}>FAQs</Link>
+          <Link className="hover:underline" href={"/guides"}>Guides</Link>
+          <Link className="hover:underline" href={"/tracking"}>Order Tracking</Link>
+          <Link className="hover:underline" href={"/warranty"}>Warranty</Link>
         </div>
-        <div className="flex flex-col">
-          <span>Legal</span>
-          <span>Privacy Policy</span>
-          <span>Terms And Conditions</span>
-          <span>Refund Policy</span>
+        <div className="flex flex-col space-y-1 max-w-fit">
+          <span className="text-slate-100 font-semibold">Legal</span>
+          <Link className="hover:underline" href={"/privacy"}>Privacy Policy</Link>
+          <Link className="hover:underline" href={"/terms"}>Terms And Conditions</Link>
+          <Link className="hover:underline" href={"/refund"}>Refund Policy</Link>
         </div>
-        <div className="flex flex-col">
-          <span>Status</span>
-          <span>Services Status</span>
-          <span>Report A Problem</span>
-          <span>Bug Bounty</span>
+        <div className="flex flex-col space-y-1 max-w-fit">
+          <span className="text-slate-100 font-semibold">Status</span>
+          <Link className="hover:underline" href={"/status"}>Services Status</Link>
+          <Link className="hover:underline" href={"/problem"}>Report A Problem</Link>
+          <Link className="hover:underline" href={"/bug"}>Bug Bounty</Link>
         </div>
       </div>
       <div className="flex flex-col items-center">
@@ -43,10 +43,20 @@ export default function Footer() {
             height={32}
           />
         </Link>
-        <span>© Dope 2023, All Rights Reserved.</span>
-        <span className="mt-1 text-xs">
-          Made With <i className="bi bi-heart-fill text-red-600"></i> By Unhot.
-        </span>
+        <div className="flex flex-col items-center border-t border-slate-700 w-11/12">
+          <span className="mt-3 text-gray-500">
+            © 2023 Dope™. All Rights Reserved.
+          </span>
+          <span className="mt-1 text-xs text-gray-500">
+            Made With <i className="bi bi-heart-fill text-red-600"></i> By
+            Unhot.
+          </span>
+          <div className="flex flex-row w-1/4 justify-around mt-2">
+            <i className="bi bi-instagram text-gray-500 hover:text-slate-50"></i>
+            <i className="bi bi-telegram text-gray-500 hover:text-slate-50"></i>
+            <i className="bi bi-twitter text-gray-500 hover:text-slate-50"></i>
+          </div>
+        </div>
       </div>
     </div>
   );

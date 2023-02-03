21f142910ed6210838e98e5392edf6c7479bd948
UNHOT <Serverings@protonmail.com>
2023-02-03 05:43:08 -0800

    Initialize.

---
+++ app/signin/page.tsx
@@ -0,0 +1,58 @@
+import Button from "@/components/Button";
+import Link from "next/link";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="border border-slate-50 rounded-sm">
+      <div className="flex flex-col justify-evenly items-center p-6">
+        <h1>Sign in / Sign Up</h1>
+        <div className="flex flex-col w-72 items-center">
+          <form className="w-full mb-2 mt-4">
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="email"
+                name="floating_email"
+                id="floating_email"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="floating_email"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Email address
+              </label>
+            </div>
+            <div className="relative z-0 w-full group">
+              <input
+                type="password"
+                name="floating_password"
+                id="floating_password"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="floating_password"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Password
+              </label>
+            </div>
+          </form>
+          <Button data="Sign In" type="submit" />
+        </div>
+        <div className="flex flex-col">
+          <span>
+            Forget Email/Password? <Link href={"/"}>Recovery</Link>
+          </span>
+          <span>
+            Do Not Have An Account? <Link href={"/signup"}>Create Account</Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}

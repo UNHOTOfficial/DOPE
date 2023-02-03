a8dc77326193fb63106d8f82aabf0d7b0c1ceb8e
UNHOT <Serverings@protonmail.com>
2023-02-03 12:42:30 -0800

    Minor Fixes.

--- app/[product]/page.tsx
+++ app/[product]/page.tsx
@@ -1,7 +1,9 @@
+import BreadCrumb from "@/components/BreadCrumb";
 import Button from "@/components/Button";
 import ProductColors from "@/components/ProductColors";
 import Sizes from "@/components/Sizes";
 import fetcher from "@/services/fetch";
+import { log } from "console";
 import Image from "next/image";
 import React from "react";
 
@@ -9,9 +11,10 @@ export default async function page(context: any) {
   const id = context.params.product;
   const productArray = await fetcher(`/api/products/${id}`);
   const product = productArray[0];
-
+  log(product);
   return (
-    <div>
+    <div className="space-y-3">
+      <BreadCrumb />
       <Image
         className="w-full mx-auto"
         src={product.photo}
@@ -19,16 +22,25 @@ export default async function page(context: any) {
         width={428}
         height={428}
       />
-      <h1>{product.title}</h1>
-      {/* <div><span>Colors:</span><ProductColors Colors={product?.colors}/></div> */}
-      <div>
+      <h1 className="text-3xl font-semibold mt-1">{product.title}</h1>
+      <div className="flex flex-row">
+        <span>Colors:</span>
+        <ProductColors Colors={product?.colors} />
+      </div>
+      <div className="flex flex-row">
         <span>Sizes:</span>
-        <Sizes />
+        <Sizes sizes={product.sizes} />
       </div>
       <p>{product.overview}</p>
       <div className="flex flex-col w-full items-stretch">
-        <Button addedClass={"w-80"} data="Add To Cart" type="button" />
-        <Button addedClass={"w-80"} data="Add To Wishlist" type="button" />
+        <Button
+          addedClass={
+            "w-80 my-1 bg-gray-700 text-white dark:bg-gray-100 dark:text-black"
+          }
+          data="Add To Cart"
+          type="button"
+        />
+        <Button addedClass={"w-80 my-0"} data="Add To Wishlist" type="button" />
       </div>
     </div>
   );

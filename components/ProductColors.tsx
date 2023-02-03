a2f55b392a18582fd8a63b0f28f5b895d5293234
UNHOT <Serverings@protonmail.com>
2023-02-03 12:43:28 -0800

    Handle One Color.

--- components/ProductColors.tsx
+++ components/ProductColors.tsx
@@ -3,20 +3,35 @@ import React from "react";
 export default function ProductColors(colors: any) {
   return (
     <div>
-      {colors.colors.map((color: any) => (        
+      {colors.length > 1 ? (
+        colors.colors.map((color: any) => (
+          <span
+            key={color.title}
+            className="border border-black"
+            style={{
+              height: "16px",
+              width: "16px",
+              backgroundColor: color,
+              borderRadius: "50%",
+              display: "inline-block",
+              marginRight: "0.2rem",
+            }}
+          ></span>
+        ))
+      ) : (
         <span
-          key={color.title}
+          key={colors}
           className="border border-black"
           style={{
             height: "16px",
             width: "16px",
-            backgroundColor: color,
+            backgroundColor: colors[0],
             borderRadius: "50%",
             display: "inline-block",
             marginRight: "0.2rem",
           }}
         ></span>
-      ))}
+      )}
     </div>
   );
 }

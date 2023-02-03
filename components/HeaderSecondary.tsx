7d83efdfe65cda56a70c815e21a9a299c654d367
UNHOT <Serverings@protonmail.com>
2023-02-03 14:07:10 -0800

    Add Aria Label.

--- components/HeaderSecondary.tsx
+++ components/HeaderSecondary.tsx
@@ -5,10 +5,10 @@ export default function HeaderSecondary() {
   return (
     <div className="flex flex-row items-center justify-around py-2 bg-slate-900 border-y border-gray-700">
       <div className="flex flex-row w-28 justify-evenly">
-        <Link href={"signin"}>
+        <Link href={"signin"} aria-label="Sign In To Your Account">
           <i className="bi bi-person" style={{ fontSize: "1.5rem" }}></i>
         </Link>
-        <Link href={"cart"}>
+        <Link href={"cart"} aria-label="View Your Shopping Cart.">
           <i className="bi bi-bag" style={{ fontSize: "1.5rem" }}></i>
         </Link>
       </div>

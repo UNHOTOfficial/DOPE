16aaaddb966d0c0258b16d47a4b15b164b3dc9a4
UNHOT <Serverings@protonmail.com>
2023-02-03 06:47:01 -0800

    Fix Bugs.

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,21 +4,7 @@ import fetcher from "@/services/fetch";
 import React from "react";
 
 export default async function page() {
-  const cartItems = [
-    {
-      id: 1,
-      photo: "https://i.imgur.com/vudDqr9.png",
-      title: "Chelsea Boots",
-      price: 399,
-      rate: 4.7,
-      count: 3786,
-      isFresh: true,
-      isBestSelling: false,
-      colors: ["#D27C3F"],
-      overview:
-        "The secret to spotting the perfect Chelsea boots is to look for those that are timeless, crafted from a hardy, long-lasting material and have a unique detail. Ticking all the right boxes, Proenza Schouler's pair has been made in Italy from black leather and detailed with contrasting white stitching. They're equipped with elasticated panels at the sides for comfort, plus handy pull tabs at the back.",
-    },
-  ]
+  const cartItems = await fetcher("/api/cart");
 
   let prices = {
     products: 0,

7c61559b060458e4ecc0c21f7eec3102a0b2681f
UNHOT <Serverings@protonmail.com>
2023-02-03 05:40:15 -0800

    Fix Error Pages.

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Button from "@/components/Button";
 import React from "react";
 
 export default function error({
@@ -9,5 +10,11 @@ export default function error({
   error: Error;
   reset: () => void;
 }) {
-  return <div>{error.message}</div>;
+  console.error(error.message);
+  return (
+    <div className="flex flex-col items-center justify-center h-52">
+      <span className="mb-2">Something Went Wrong.</span>
+      <Button data={"Retry"} type="reset" addedClass="" />
+    </div>
+  );
 }

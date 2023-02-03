1db0d113569b4ece10c3550b9230783a598df3c9
UNHOT <Serverings@protonmail.com>
2023-02-03 12:43:38 -0800

    Add Component.

--- components/Sizes.tsx
+++ components/Sizes.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
-export default function Sizes() {
-  return <div>Sizes</div>;
+export default function Sizes({ sizes }: any) {
+  return (
+    <div>
+      {sizes.length > 1 ? (
+        sizes.map((size: any) => (
+          <span
+            key={size}
+            className="px-2 ring-1 ring-gray-100 dark:ring-gray-700 rounded mx-1"
+          >
+            {size}
+          </span>
+        ))
+      ) : (
+        <span
+          key={sizes[0]}
+          className="px-2 ring-1 ring-gray-100 dark:ring-gray-700 rounded mx-1"
+        >
+          {sizes[0]}
+        </span>
+      )}
+    </div>
+  );
 }

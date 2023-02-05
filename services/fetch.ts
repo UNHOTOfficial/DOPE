912a58065e609e47e0534b6d6c4077a88f7b6b80
UNHOT <Serverings@protonmail.com>
2023-02-05 03:43:04 -0800

    Add Fetch Types.

--- services/fetch.ts
+++ services/fetch.ts
@@ -1,11 +1,35 @@
 import { config } from "./url";
 
-export default async function fetcher(endpoint: string) {
+export default async function fetcher(endpoint: string, fetchType: string) {
   const url = config.url;
-  try {
-    const req = await fetch(url + endpoint);
-    return await req.json();
-  } catch (error: any) {
-    throw new Error(error.toString());
+  let response;
+
+  switch (fetchType) {
+    case "static":
+      const staticFetch = await fetch(url + endpoint);
+      if (!staticFetch.ok) {
+        throw new Error("Failed To Fetch Data.");
+      }
+      response = staticFetch.json();
+      break;
+
+    case "revalidate":
+      const revalidate = await fetch(url + endpoint, {
+        next: { revalidate: 10 },
+      });
+      if (!revalidate.ok) {
+        throw new Error("Failed To Fetch Data.");
+      }
+      response = revalidate.json();
+      break;
+
+    case "dynamic":
+      const dynamic = await fetch(url + endpoint, { cache: "no-store" });
+      if (!dynamic.ok) {
+        throw new Error("Failed To Fetch Data.");
+      }
+      response = dynamic.json();
+      break;
   }
+  return response;
 }

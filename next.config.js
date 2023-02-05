864928185b83d904f6779956d22b11ade8c67eff
UNHOT <Serverings@protonmail.com>
2023-02-05 01:04:23 -0800

    PWA Fix.

--- next.config.js
+++ next.config.js
@@ -5,7 +5,14 @@ const withPWA = require("next-pwa")({
   skipWaiting: true,
   disable: false,
   sw: "service-worker.js",
-  buildExcludes: [/middleware-manifest\.json$/],
+  buildExcludes: [
+    /\/*server\/middleware-chunks\/[0-9]*[a-z]*[A-Z]*\.js$/,
+    /middleware-manifest\.json$/,
+    /middleware-runtime\.js$/,
+    /_middleware\.js$/,
+    /^.+\\_middleware\.js$/,
+],
+publicExcludes: ['!robots.txt'],
   runtimeCaching,
 });
 

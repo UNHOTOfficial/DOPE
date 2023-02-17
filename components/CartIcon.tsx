8d003286de3dade9b93d74d0961dd7da1df30dc9
UNHOT <Serverings@protonmail.com>
2023-02-17 11:47:21 +0330

    Fix Bugs.

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -6,7 +6,10 @@ export default function CartIcon({ status }: { status: any }) {
   if (status.data.length > 1) {
     return (
       <div className="inline-flex relative">
-        <div className="absolute inline-block top-0 right-0 bottom-auto left-auto translate-x-2/4 translate-y-full rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 p-1 text-xs bg-red-600 rounded-full z-10"></div>
+        <div
+          className="absolute inline-block top-0 right-0 bottom-auto left-auto translate-x-2/4 translate-y-full rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 p-1 text-xs bg-red-600 rounded-full"
+          style={{ zIndex: "5" }}
+        ></div>
         <i className="bi bi-bag" style={{ fontSize: "1.5rem" }}></i>
       </div>
     );
